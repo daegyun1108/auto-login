411c0e659cbd0224b7a7cf62b734b961d051324a
Posweat <75527280+daegyun1108@users.noreply.github.com>
2022-03-20 20:03:51 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,35 @@
+import time
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+
+driver.implicitly_wait(10) 
+
+driver.get("https://www.naver.com")
+
+from selenium.webdriver.common.by import By
+driver.find_element(By.CLASS_NAME,'link_login').click()
+
+user = {'id':'id','pw':'id'} #id pw 입력
+
+#driver.find_element(By.ID,'id').send_keys(user['id'])
+#driver.find_element(By.ID,'pw').send_keys(user['pw'])
+#driver.find_element(By.CLASS_NAME, 'btn_global').click()
+
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+import pyperclip
+
+driver.find_element(By.ID,'id').click()
+pyperclip.copy(user['id'])
+ActionChains(driver).key_down(Keys.CONTROL).send_keys('v').key_up(Keys.CONTROL).perform()
+
+driver.find_element(By.ID,'pw').click()
+pyperclip.copy(user['pw'])
+ActionChains(driver).key_down(Keys.CONTROL).send_keys('v').key_up(Keys.CONTROL).perform()
+
+driver.find_element(By.ID,'log.login').click()
+
+while True:
+    time.sleep(1)
